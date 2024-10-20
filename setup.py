c9329a2c9d4618e456451c9fa63102e25087fbbc
Cory Fitz <coryalanfitz@gmail.com>
2024-10-20 22:23:52 +0100

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="moderne",
-    version="0.0.1",
+    version="0.0.2",
     author="Cory Fitz",
     author_email="coryalanfitz@gmail.com",
     description="Moderne Web Framework",
